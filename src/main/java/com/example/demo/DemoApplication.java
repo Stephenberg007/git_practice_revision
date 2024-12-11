657b70d5506a3ba249f74989708fcd9c221f2868
Aman <krmauryaaman@gmail.com>
2024-12-11 12:14:29 +0530

    adding files to Central Repo

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+	
+System.out.println("1st Line");
+System.out.println("2nd Line");
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		int a =100;
+		int b =1010;
+		int i=10;
+		float i =22.45f;
+double price = 1000.0;
+boolean b1 = true;
+		boolean isUpdated = false;
+
+	int a1 =100;
+	int b1 =251;
+		int c1 =351;
+
+
+		system.out.println(i);
+
+
+	}
+
+}
