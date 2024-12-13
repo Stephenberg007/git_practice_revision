0d340b5177f1192bdad9b680f0ea259a8baed5eb
Aman <krmauryaaman@gmail.com>
2024-12-13 08:46:01 +0530

    added changes

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,10 +10,7 @@ System.out.println("1st Line");
 System.out.println("2nd Line");
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		int a =100;
-		int b =1010;
-		int i=10;
-		float i =22.45f;
+		
 double price = 1000.0;
 boolean b1 = true;
 		boolean isUpdated = false;
